63315b5b79c439c95c9346e1203268af98cb3f04
leungpo <polly.leung@oocl.com>
2022-11-29 19:40:39 +0800

    init: init PosMachine function

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -6,4 +6,20 @@ public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
     }
+    public int calculateSubTotalItemPrice(List<BarcodeAmount> barcodeAmount, String barcode, Item[] allItems){
+        return 0;
+    }
+    public int calculateTotalPrice(List<BarcodeAmount> barcodeAmount,List<String> barcodes, Item[] allItems){
+        return 0;
+    }
+    public String printReceiptLine(List<BarcodeAmount> barcodeAmount,String barcode, Item[] allItems){
+        return null;
+    }
+    public String printResult(List<BarcodeAmount> barcodeAmount,List<String> barcodes, Item[] allItems){
+        return null;
+    }
+    public BarcodeAmount countItemAmount(List<String>barcodes){
+        return null;
+    }
+
 }
