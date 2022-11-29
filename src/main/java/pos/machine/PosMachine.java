249cc34828a6c34f0c7b3dfa862ceb7bd6fe67fd
WONGCH9 <wongch9@oocl.com>
2022-11-29 20:05:29 +0800

    feat printReceipt

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,14 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        return null;
+
+        Receipt receipt = new Receipt(ItemDataLoader.decodeToItems(barcodes));
+
+        return receipt.generateReport();
+
     }
 }
