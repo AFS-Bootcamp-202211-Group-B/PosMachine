f62d6d2973e82d6c625219a92352def96f20d73f
leungpo <polly.leung@oocl.com>
2022-11-29 21:02:36 +0800

    feat: finish printResult

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -7,7 +7,7 @@ public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
     }
-    public int getItemPrice(String barcode, Item[] allItems){
+    public int getItemPrice(String barcode, List<Item> allItems){
         for (Item item:allItems){
             if(item.getBarcode().equals(barcode)){
                 return item.getPrice();
@@ -15,7 +15,7 @@ public class PosMachine {
         }
         return 0;
     }
-    public String getItemName(String barcode, Item[] allItems){
+    public String getItemName(String barcode, List<Item> allItems){
         for (Item item:allItems){
             if(item.getBarcode().equals(barcode)){
                 return item.getName();
@@ -23,26 +23,34 @@ public class PosMachine {
         }
         return null;
     }
-    public int calculateSubTotalItemPrice(BarcodeAmount barcodeAmount, Item[] allItems){
+    public int calculateSubTotalItemPrice(BarcodeAmount barcodeAmount, List<Item> allItems){
         int price = getItemPrice(barcodeAmount.getBarcode(),allItems);
         int subTotal = price * barcodeAmount.getAmount();
         return subTotal;
     }
-    public int calculateTotalPrice(List<BarcodeAmount> barcodeAmounts, Item[] allItems){
+    public int calculateTotalPrice(List<BarcodeAmount> barcodeAmounts, List<Item> allItems){
         int totalPrice = 0;
         for(BarcodeAmount barcodeAmount: barcodeAmounts){
             totalPrice += calculateSubTotalItemPrice(barcodeAmount,allItems);
         }
         return totalPrice;
     }
-    public String printReceiptLine(BarcodeAmount barcodeAmount, Item[] allItems){
+    public String printReceiptLine(BarcodeAmount barcodeAmount, List<Item> allItems){
         String itemName = getItemName(barcodeAmount.getBarcode(),allItems);
         int unitPrice = getItemPrice(barcodeAmount.getBarcode(), allItems);
         String resultLine = "Name: " + itemName + ", Quantity: " + barcodeAmount.getAmount() + ", Unit price: " + unitPrice + " (yuan), Subtotal: " + calculateSubTotalItemPrice(barcodeAmount,allItems);
         return resultLine;
     }
-    public String printResult(List<BarcodeAmount> barcodeAmount, Item[] allItems){
-        return null;
+    public String printResult(List<BarcodeAmount> barcodeAmounts, List<Item> allItems){
+        String recipt = "***<store earning no money>Receipt ***" + System.lineSeparator();
+        for(BarcodeAmount barcodeAmount: barcodeAmounts){
+            recipt += printReceiptLine(barcodeAmount,allItems) + System.lineSeparator();
+        }
+        recipt += "----------------------" + System.lineSeparator() +
+                "Total: " + calculateTotalPrice(barcodeAmounts,allItems) + "(yuan)" + System.lineSeparator()
+                + "**********************";
+
+        return recipt;
     }
     public List<BarcodeAmount> countItemAmount(List<String>barcodes){
         List<BarcodeAmount>barcodeAmounts = new ArrayList<BarcodeAmount>();
