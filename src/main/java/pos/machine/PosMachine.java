7d62066b1685cfdc48be12e76fda9a592e02fc7a
Joyce Li <joyceli214@yahoo.com.hk>
2022-11-29 16:48:57 +0800

    feat: finish getItem

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,15 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
+    }
+
+    private Item getItem(String barcode, List<Item> items) {
+        return items.stream().filter(item -> item.getBarcode().equals(barcode)).findFirst().orElse(null);
+
     }
 }
