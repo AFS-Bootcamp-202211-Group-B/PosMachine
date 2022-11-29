aaf6b3716e9bf3c68c21458ae70481e885fb54d8
mansan1230 <mansantamm@gmail.com>
2022-11-29 20:36:19 +0800

    Done All

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,67 @@
 package pos.machine;
-
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        return null;
+        List<Item> itemsWithDetail = getItemInfos(barcodes);
+        Receipt receipt = calculateCost(itemsWithDetail);
+        return generateTotal(receipt);
+    }
+
+    private String generateTotal(Receipt receipt) {
+        String printedReceipt = generateLine(receipt);
+        printedReceipt += generateTotalLine(receipt);
+        return printedReceipt;
+    }
+
+    private String generateLine(Receipt receipt) {
+        String itemsDetail = "***<store earning no money>Receipt***\n";
+        for (ReceiptItem receiptItem : receipt.getItemDetail()) {
+            itemsDetail += "Name: " + receiptItem.getName() + ", " + "Quantity: " + receiptItem.getQuantity() + ", " + "Unit price: "
+                    + receiptItem.getUnitPrice() + " (yuan), Subtotal: " + receiptItem.getSubTotal() + " (yuan)\n";
+        }
+        return itemsDetail;
+    }
+
+    private String generateTotalLine(Receipt receipt) {
+        return "----------------------\n" +
+                "Total: " + receipt.getTotalPrice() + " (yuan)" +
+                "\n**********************";
+    }
+
+    private List<Item> getItemInfos(List<String> barcodes){
+        List<Item> database = ItemDataLoader.loadAllItems();
+        List<Item> itemsWithDetail = new ArrayList<>();
+        for (String barcode : barcodes){
+            for (int i = 0; i < database.size(); i++){
+                if (barcode.equals(database.get(i).getBarcode())) {
+                    itemsWithDetail.add(database.get(i));
+                }
+            }
+        }
+        return itemsWithDetail;
+    }
+    private Receipt calculateCost(List<Item> itemsWithDetail){
+        List<ReceiptItem> receiptItems = calculateReceiptItems(itemsWithDetail);
+        int totalPrice = calculateTotalPrice(receiptItems);
+        return new Receipt(receiptItems, totalPrice);
+    }
+    private List<ReceiptItem> calculateReceiptItems(List<Item> itemsWithDetail) {
+        List<ReceiptItem> receiptItems = new ArrayList<>();
+        List<Item> itemsWithDetail_distinct = itemsWithDetail.stream().distinct().collect(Collectors.toList());
+        for (Item item : itemsWithDetail_distinct) {
+            int quantity = Collections.frequency(itemsWithDetail, item);
+            receiptItems.add(new ReceiptItem(item.getName(), quantity, item.getPrice(), item.getPrice() * quantity));
+        }
+        return receiptItems;
+    }
+    private int calculateTotalPrice(List<ReceiptItem> receiptItems){
+        int totalPrice = 0;
+        for (ReceiptItem receiptItem : receiptItems){
+            totalPrice = totalPrice + receiptItem.getSubTotal();
+        }
+        return totalPrice;
     }
 }
+
---
+++ src/main/java/pos/machine/ReceiptItem.java
@@ -0,0 +1,41 @@
+package pos.machine;
+
+
+public class ReceiptItem {
+    private String name;
+    private int quantity;
+    private int unitPrice;
+    private int subTotal;
+
+    public ReceiptItem(String name, int quantity, int unitPrice, int subTotal) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.subTotal = subTotal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public int getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(int subTotal) {
+        this.subTotal = subTotal;
+    }
+}
