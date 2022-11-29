571c81b72c171a2596dde3bb1c4e80bd70c4f60b
tokaho <tokaho2000@gmail.com>
2022-11-29 17:58:21 +0800

    [feat] finish mapBarcodeToDetails

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,23 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import pos.machine.Item;
+import pos.machine.ItemDataLoader;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+        List<Item> allItems = ItemDataLoader.loadAllItems();
         return null;
+    }
+
+
+    public Item mapBarcodeToDetails(String Barcode, List<Item> allItems){
+        return allItems.stream()
+                .filter(item-> item.getBarcode().equals(Barcode))
+                .collect(Collectors.toList()).get(0);
     }
 }
