763bfed3f50bf950893c9e2af6bbc3bb0c11476b
leungpo <polly.leung@oocl.com>
2022-11-29 20:16:53 +0800

    feat: finish countItemAmount

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,5 +1,6 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PosMachine {
@@ -18,8 +19,24 @@ public class PosMachine {
     public String printResult(List<BarcodeAmount> barcodeAmount,List<String> barcodes, Item[] allItems){
         return null;
     }
-    public BarcodeAmount countItemAmount(List<String>barcodes){
-        return null;
+    public List<BarcodeAmount> countItemAmount(List<String>barcodes){
+        List<BarcodeAmount>barcodeAmounts = new ArrayList<BarcodeAmount>();
+        for (String barcode: barcodes){
+            boolean isCount = false;
+            for(BarcodeAmount barcodeAmount:barcodeAmounts){
+                if(barcodeAmount.getBarcode().equals(barcode)){
+                    barcodeAmount.setAmount(barcodeAmount.getAmount()+1);
+                    isCount = true;
+                    break;
+                }
+            }
+            if(!isCount){
+                BarcodeAmount newBarcodeAmount = new BarcodeAmount(barcode,1);
+                barcodeAmounts.add(newBarcodeAmount);
+            }
+        }
+        System.out.println(barcodeAmounts);
+        return barcodeAmounts;
     }
 
 }
