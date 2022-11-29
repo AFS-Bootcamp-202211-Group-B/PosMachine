a4b32db5e842545e33ab77a409bd519165344873
tokaho <tokaho2000@gmail.com>
2022-11-29 18:39:48 +0800

    [feat] finish countProduct

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,6 +1,7 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -10,10 +11,18 @@ import pos.machine.ItemDataLoader;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        
-        return null;
+        List<BarcodeGroup> barcodeCount = countProduct(barcodes);
+        List<ItemGroup> itemAggResult = aggregateAllProducts(barcodeCount);            
+        return receipt(itemAggResult);
     }
 
+    public List<BarcodeGroup> countProduct(List<String> barcodes){
+        return barcodes.stream()
+            .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
+            .entrySet().stream()
+            .map(count-> new BarcodeGroup(count.getKey(), count.getValue().intValue()))
+            .collect(Collectors.toList());    
+    }
     public List<ItemGroup> aggregateAllProducts(List<BarcodeGroup> barcodeGroups){
         List<Item> allItems = ItemDataLoader.loadAllItems();
         return barcodeGroups.stream().map(barcodeGroup->aggregateProduct(barcodeGroup,allItems))
@@ -31,6 +40,8 @@ public class PosMachine {
                 .filter(item-> item.getBarcode().equals(Barcode))
                 .collect(Collectors.toList()).get(0);    
     }
+
+
 
     public int sumCost(List<ItemGroup> itemGroups){
         return itemGroups.stream().map(itemGroup->itemGroup.getSubTotal()).reduce(0, Integer::sum);
